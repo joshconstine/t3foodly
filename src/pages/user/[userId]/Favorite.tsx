bcd8ff28b3d4bcca0a51f0707242bedebcdc4dda
joshua constine <joshua.constine97@gmail.com>
2023-02-10 14:44:16 -0800

    added new favorites query to the profile page, added output validation for zipcode search

--- src/pages/user/[userId]/Favorite.tsx
+++ src/pages/user/[userId]/Favorite.tsx
@@ -2,13 +2,12 @@ import Link from "next/link";
 import { api } from "../../../utils/api";
 
 const Favorite = ({ restaurantId }: { restaurantId: string }) => {
-    const restaurant = api.restaurant.getById.useQuery({ id: restaurantId })
-    return (
-        <div>
-            {restaurant.data?.name}
-
-        </div>
-    );
-}
+  const restaurant = api.restaurant.getById.useQuery({ id: restaurantId });
+  return (
+    <div>
+      <Link href={`/restaurant/${restaurantId}`}>{restaurant.data?.name}</Link>
+    </div>
+  );
+};
 
 export default Favorite;
--- src/server/api/routers/restaurant.ts
+++ src/server/api/routers/restaurant.ts
@@ -4,43 +4,60 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
 export const restaurantRouter = createTRPCRouter({
-    restaurantGreeting: publicProcedure
-        .query(() => {
-            return {
-                greeting: `Hello from restaurants`,
-            };
-        }),
+  restaurantGreeting: publicProcedure.query(() => {
+    return {
+      greeting: `Hello from restaurants`,
+    };
+  }),
 
-    getAll: publicProcedure.query(({ ctx }) => {
-        return ctx.prisma.restaurant.findMany()
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.restaurant.findMany();
+  }),
+  getById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ input, ctx }) => {
+      return ctx.prisma.restaurant.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
     }),
-    getById: publicProcedure.input(z.object({ id: z.string() })).query(({ input, ctx }) => {
-        return ctx.prisma.restaurant.findUnique({
-            where: {
-                id: input.id
-            }
+  getByZipcode: publicProcedure
+    .input(z.object({ zipcode: z.string().nullable() }))
+    .output(
+      z.array(
+        z.object({
+          restaurantName: z.string(),
+          id: z.number(),
         })
-    }),
-    getByZipcode: publicProcedure.input(z.object({ zipcode: z.string().nullable() })).query(({ input, ctx }) => {
-        const options = {
-            method: 'GET',
-            url: `https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/zipcode/${input.zipcode}/0`,
-            headers: {
-                'X-RapidAPI-Key': process.env.XRAPID_KEY,
-                'X-RapidAPI-Host': process.env.XRAPID_HOST
-            }
-        };
+      )
+    )
+    .query(({ input, ctx }) => {
+      const options = {
+        method: "GET",
+        url: `https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/zipcode/${input.zipcode}/0`,
+        headers: {
+          "X-RapidAPI-Key": process.env.XRAPID_KEY,
+          "X-RapidAPI-Host": process.env.XRAPID_HOST,
+        },
+      };
 
-        return axios.request(options).then(function (response) {
-            return response.data.restaurants ? response.data.restaurants : []
-        }).catch(function (error) {
-            console.error(error);
-            return []
+      return axios
+        .request(options)
+        .then(function (response) {
+          return response.data.restaurants
+            ? response.data.restaurants.map((elem: any) => {
+                return { id: elem.id, restaurantName: elem.restaurantName };
+              })
+            : [];
+        })
+        .catch(function (error) {
+          console.error(error);
+          return [];
         });
     }),
 
-
-    getSecretMessage: protectedProcedure.query(() => {
-        return "you can now see this secret message!";
-    }),
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
 });
