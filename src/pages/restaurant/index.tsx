9dd9fd4e169f764291dfc1242c614e0008efd8ed
joshua constine <joshua.constine97@gmail.com>
2023-03-14 19:55:20 -0700

    new load script

--- src/pages/restaurant/index.tsx
+++ src/pages/restaurant/index.tsx
@@ -13,9 +13,16 @@ import { Autocomplete } from "../../components/forms/Autocomplete";
 import Map, { IMarker, Point } from "../../components/forms/Map";
 import RestaurantSearchForm from "../../components/RestaurantsSearchForm";
 import FocusedRestaurantCard from "../../components/RestaurantCards/FocusedRestaurantCard";
+import { useLoadScript } from "@react-google-maps/api";
 const center = {
   lat: 32.715,
   lng: -117.16,
+};
+const scriptOptions = {
+  googleMapsApiKey: process.env.NEXT_PUBLIC_PLACES_KEY
+    ? process.env.NEXT_PUBLIC_PLACES_KEY
+    : "",
+  libraries: ["places"],
 };
 const Restaurant: NextPage = () => {
   const router = useRouter();
@@ -24,6 +31,8 @@ const Restaurant: NextPage = () => {
   const [state, setState] = useState<string>("");
   const [markers, setMarkers] = useState<IMarker[]>([]);
   const [mapCenter, setMapCenter] = useState(center);
+  //@ts-ignore
+  const { isLoaded, loadError } = useLoadScript(scriptOptions);
 
   const [focusedRestaurant, setFocusedRestaurant] = useState<string | null>(
     null
@@ -127,79 +136,82 @@ const Restaurant: NextPage = () => {
     }
     return 0;
   }, [apiRestaurants.data, dbRestaurants.data]);
-  return (
-    <>
-      <Head>
-        <title>Foodley</title>
-        <meta name="description" content="find great local restaurants" />
-        <link rel="icon" href="/favicon.ico" />
-        <script
-          defer
+
+  if (loadError) return "Error";
+
+  if (isLoaded)
+    return (
+      <>
+        <Head>
+          <title>Foodley</title>
+          <meta name="description" content="find great local restaurants" />
+          <link rel="icon" href="/favicon.ico" />
+          {/* <script
           src={`https://maps.googleapis.com/maps/api/js?key=${
             process.env.NEXT_PUBLIC_PLACES_KEY
               ? process.env.NEXT_PUBLIC_PLACES_KEY
               : ""
           }&libraries=places`}
-        ></script>
-      </Head>
-      <Layout>
-        <section className="py-12">
-          <div className="mx-auto px-4 sm:px-6 lg:px-8 ">
-            <div className="flex w-full flex-col  items-center gap-2 ">
-              <div className=" mx-2 flex w-full flex-col-reverse items-center justify-between gap-4 rounded-lg border-2 px-8 py-4 md:flex-row">
-                <div>
-                  <span>Dont see your favorite</span>
+        ></script> */}
+        </Head>
+        <Layout>
+          <section className="py-12">
+            <div className="mx-auto px-4 sm:px-6 lg:px-8 ">
+              <div className="flex w-full flex-col  items-center gap-2 ">
+                <div className=" mx-2 flex w-full flex-col-reverse items-center justify-between gap-4 rounded-lg border-2 px-8 py-4 md:flex-row">
                   <div>
-                    <Link href="/restaurant/create">
-                      <motion.div
-                        className="rounded-full bg-secondary py-2 px-4 font-bold text-white"
-                        whileHover={{ scale: 1.1 }}
-                        whileTap={{ scale: 0.9 }}
-                      >
-                        Add restaurant
-                      </motion.div>
-                    </Link>
+                    <span>Dont see your favorite</span>
+                    <div>
+                      <Link href="/restaurant/create">
+                        <motion.div
+                          className="rounded-full bg-secondary py-2 px-4 font-bold text-white"
+                          whileHover={{ scale: 1.1 }}
+                          whileTap={{ scale: 0.9 }}
+                        >
+                          Add restaurant
+                        </motion.div>
+                      </Link>
+                    </div>
                   </div>
+                  <RestaurantSearchForm
+                    setCity={setCity}
+                    setState={setState}
+                    city={city}
+                    state={state}
+                    setMapCenter={setMapCenter}
+                  />
+                  <div></div>
                 </div>
-                <RestaurantSearchForm
-                  setCity={setCity}
-                  setState={setState}
-                  city={city}
-                  state={state}
-                  setMapCenter={setMapCenter}
-                />
-                <div></div>
-              </div>
 
-              <div className=" flex w-full  flex-col justify-center md:h-special md:flex-row ">
-                <div className="lg flex w-full flex-col gap-4 md:w-860  md:min-w-860 md:overflow-auto ">
-                  <div className="min-w-96 flex flex-col gap-4">
-                    <h1 className="text-l  relative font-bold text-primary">
-                      {`${resultsNum} Restaurants`}
-                    </h1>
+                <div className=" flex w-full  flex-col justify-center md:h-special md:flex-row ">
+                  <div className="lg flex w-full flex-col gap-4 md:w-860  md:min-w-860 md:overflow-auto ">
+                    <div className="min-w-96 flex flex-col gap-4">
+                      <h1 className="text-l  relative font-bold text-primary">
+                        {`${resultsNum} Restaurants`}
+                      </h1>
+                    </div>
+                    {focusedRestaurant && (
+                      <FocusedRestaurantCard restaurantId={focusedRestaurant} />
+                    )}
+                    <RestaurantResults
+                      dbRestaurants={dbRestaurants.data}
+                      apiRestaurants={apiRestaurants.data}
+                    />
                   </div>
-                  {focusedRestaurant && (
-                    <FocusedRestaurantCard restaurantId={focusedRestaurant} />
-                  )}
-                  <RestaurantResults
-                    dbRestaurants={dbRestaurants.data}
-                    apiRestaurants={apiRestaurants.data}
-                  />
-                </div>
-                <div className="min-w-96 relative left-0 top-0 h-full w-full">
-                  <Map
-                    mapCenter={mapCenter}
-                    markers={markers}
-                    setFocusedRestaurant={setFocusedRestaurant}
-                  />
+                  <div className="min-w-96 relative left-0 top-0 h-full w-full">
+                    <Map
+                      mapCenter={mapCenter}
+                      markers={markers}
+                      setFocusedRestaurant={setFocusedRestaurant}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </section>
-      </Layout>
-    </>
-  );
+          </section>
+        </Layout>
+      </>
+    );
 };
 
 export default Restaurant;
