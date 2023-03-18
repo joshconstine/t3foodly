d86e65da6512c5df17e87dba02f7e50d67b8f74c
joshua constine <joshua.constine97@gmail.com>
2023-03-18 12:05:28 -0700

    updated search form values

--- src/components/forms/SearchForm.tsx
+++ src/components/forms/SearchForm.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 import { useRouter } from "next/router";
-import { useLoadScript, Autocomplete } from "@react-google-maps/api";
+import { useLoadScript } from "@react-google-maps/api";
+import usePlacesAutocomplete, { getGeocode } from "use-places-autocomplete";
 
 const scriptOptions = {
   googleMapsApiKey: process.env.NEXT_PUBLIC_PLACES_KEY
@@ -13,38 +14,60 @@ export default function SearchForm() {
   const router = useRouter();
   // @ts-ignore
   const { isLoaded, loadError } = useLoadScript(scriptOptions);
-  const [autocomplete, setAutocomplete] = useState(null);
-  const inputEl = useRef(null);
 
-  // Handle the keypress for input
-  const onKeypress = (e: any) => {
-    // On enter pressed
-    if (e.key === "Enter") {
-      e.preventDefault();
-      return false;
-    }
-  };
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      /* Define search scope here */
+    },
+    debounce: 300,
+  });
+  const handleSelect =
+    ({ description }: { description: string }) =>
+    () => {
+      setValue(description, false);
+      clearSuggestions();
 
-  const onLoad = (autocompleteObj: any) => {
-    setAutocomplete(autocompleteObj);
-  };
-  let headers = new Headers();
+      getGeocode({ address: description }).then((results) => {
+        if (results.length === 0 || results[0] === undefined) return;
+        const city = results[0]?.address_components.find((elem) =>
+          elem.types.includes("locality")
+        )?.long_name;
+        const state = results[0]?.address_components.find((elem) =>
+          elem.types.includes("administrative_area_level_1")
+        )?.short_name;
+        if (city && state) {
+          router.push(`/restaurant?city=${city}&state=${state}`);
+        }
+      });
+    };
+  const renderSuggestions = () =>
+    data.map((suggestion) => {
+      const {
+        place_id,
+        structured_formatting: { main_text, secondary_text },
+      } = suggestion;
 
-  const onPlaceChanged = async () => {
-    //@ts-ignore
-    const inputs = inputEl.current?.value?.split(",");
-    const city = inputs[0].trim();
-    const state = inputs[1].trim();
+      return (
+        <li
+          key={place_id}
+          onClick={handleSelect(suggestion)}
+          style={{ cursor: "pointer" }}
+          className="py-2 px-4 hover:bg-gray-100"
+        >
+          <strong>{main_text}</strong> <small>{secondary_text}</small>
+        </li>
+      );
+    });
+  const handleInput = (e: any) => {
+    // Update the keyword of the input element
 
-    if (autocomplete) {
-      // @ts-ignore
-      const place = autocomplete.getPlace();
-      if (city && state) {
-        router.push(`/restaurant?city=${city}&state=${state}`);
-      } else if ("place_id" in place) {
-        router.push(`/restaurant/?place=${place.place_id}`);
-      }
-    }
+    setValue(e.target?.value);
   };
 
   return (
@@ -55,20 +78,21 @@ export default function SearchForm() {
 
       {isLoaded && (
         <React.Fragment>
-          <Autocomplete
-            onLoad={onLoad}
-            fields={["place_id"]}
-            onPlaceChanged={onPlaceChanged}
-            className="bg-transparent"
-          >
+          <div>
             <input
-              ref={inputEl}
-              type="text"
-              placeholder="Type keywords..."
-              className="bg-transparent py-2 px-4 focus:outline-none"
-              onKeyPress={onKeypress}
+              value={value}
+              onChange={handleInput}
+              disabled={!ready}
+              placeholder="Where are you going?"
+              className="w-full rounded-full bg-transparent py-2 px-8 focus:outline-none "
             />
-          </Autocomplete>
+            {/* We can use the "status" to decide whether we should display the dropdown or not */}
+            {status === "OK" && (
+              <div className="absolute rounded-lg bg-white p-4 shadow-lg">
+                <ul>{renderSuggestions()}</ul>
+              </div>
+            )}
+          </div>
         </React.Fragment>
       )}
     </div>
