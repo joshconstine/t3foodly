eeb8929639aea4b3e36374492391bc0101fb5e17
joshua constine <joshua.constine97@gmail.com>
2023-03-18 11:57:01 -0700

    added show username modal

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -6,11 +6,13 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import FavoriteContainer from "../components/Favorites/FavoriteContainer";
 import RestaurantCardContainer from "../components/RestaurantCards/RestaurantCardContainer";
+import Dialog from "@mui/material/Dialog";
 
 const Profile: NextPage = () => {
   const user = api.user.getUser.useQuery();
   const updateUsername = api.user.updateUsername.useMutation();
   const [isEditMode, setIsEditMode] = useState(false);
+  const [showUserNameModal, setShowUserNameModal] = useState(false);
   const favorites = api.favorite.getByUserId.useQuery({
     id: String(user.data?.id),
   });
@@ -25,6 +27,14 @@ const Profile: NextPage = () => {
   useEffect(() => {
     setSavedList(savedRestaurants.data || []);
   }, [savedRestaurants.data]);
+
+  useEffect(() => {
+    if (user.data?.username === null) {
+      setShowUserNameModal(true);
+      setIsEditMode(true);
+    }
+  }, [user.data?.username]);
+
   const handleSubmit = (e: React.SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
     const form = e.currentTarget;
@@ -55,6 +65,32 @@ const Profile: NextPage = () => {
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="container mx-auto px-4 py-10">
               <div className="-mx-4 flex flex-wrap">
+                <Dialog
+                  open={showUserNameModal}
+                  onClose={() => setShowUserNameModal(false)}
+                >
+                  <div className="flex flex-col items-center justify-center text-xl text-primary">
+                    <h1 className="text-2xl">Please enter a username</h1>
+                    {isEditMode && (
+                      <form
+                        onSubmit={handleSubmit}
+                        className="flex w-48 flex-col"
+                      >
+                        <input
+                          name="newUsername"
+                          placeholder="Username"
+                          className="w-full rounded-full bg-gray-100 py-2 px-8 focus:outline-none "
+                        ></input>
+                        <button
+                          type="submit"
+                          className="rounded-full bg-green-500 py-2 px-4 font-bold text-white hover:bg-green-700"
+                        >
+                          Save
+                        </button>
+                      </form>
+                    )}
+                  </div>
+                </Dialog>
                 <div className="mb-4 w-full px-4 md:w-1/3">
                   <div className="overflow-hidden rounded-lg bg-white shadow-lg">
                     <Image
