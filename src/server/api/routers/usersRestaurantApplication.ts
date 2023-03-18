2c5ef8396618636059bf2069fb1ecfb108201726
joshua constine <joshua.constine97@gmail.com>
2023-03-18 14:05:30 -0700

    added page for a users restaurant application and can approve/reject requests

--- src/server/api/routers/usersRestaurantApplication.ts
+++ src/server/api/routers/usersRestaurantApplication.ts
@@ -5,6 +5,13 @@ import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 export const UsersRestaurantRequest = z.object({
   restaurantId: z.string(),
 });
+export const UsersRestaurantApplication = z.object({
+  id: z.string(),
+  restaurant_id: z.string(),
+  status: z.string(),
+  created_by_user_id: z.string(),
+});
+
 export const usersRestaurantApplicationRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.usersRestaurantApplication.findMany({
@@ -50,6 +57,20 @@ export const usersRestaurantApplicationRouter = createTRPCRouter({
       return ctx.prisma.usersRestaurantApplication.delete({
         where: {
           id: input.applicationId,
+        },
+      });
+    }),
+  updateApplication: publicProcedure
+    .input(UsersRestaurantApplication)
+    .mutation(({ input, ctx }) => {
+      return ctx.prisma.usersRestaurantApplication.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          restaurant_id: input.restaurant_id,
+          status: input.status,
+          created_by_user_id: input.created_by_user_id,
         },
       });
     }),
