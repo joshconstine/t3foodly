ebbcec0de263ac05de2d50004ee4797d555c5800
joshua constine <joshua.constine97@gmail.com>
2023-07-08 13:13:19 -0700

    updated create restaurant messege

--- src/pages/restaurant/create/index.tsx
+++ src/pages/restaurant/create/index.tsx
@@ -17,10 +17,17 @@ const createRestaurant: NextPage = () => {
         <section className=" py-12">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="flex flex-col">
-              <h2 className="mb-8 text-3xl font-bold">Create Restaurant</h2>
-              <p className="mt-4 text-lg text-primary">
-                We are excited to add Your local restaurant to our system.
-                Please fill out the form below to add a restaurant.
+              <h2 className="mb-8 text-3xl font-bold">
+                Add restaurant to Foodley
+              </h2>
+              <p className="mt-4 w-1/2 text-lg text-primary">
+                We are excited to add Your restaurant to Foodley. Please fill
+                out the form below, If you are the manager of the restaurant and
+                would like to have admin permissions check the box on step 4.
+              </p>
+              <p className="mt-4 w-1/2 text-lg text-primary">
+                We review all applications internally. Thank you for helping the
+                Fooldey community!
               </p>
               <div className="flex">
                 <div className="mx-auto my-8 max-w-4xl px-4">
