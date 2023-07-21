3956f41ea3677df9f5f79842119a4121d24d52a0
joshua constine <joshua.constine97@gmail.com>
2023-07-21 14:42:06 -0700

    hotfix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -48,7 +48,7 @@ const Navbar = () => {
                 >
                   Discover
                 </Link>
-                {user.data?.role === "ADMIN" && (
+                {user.data?.role === "ADMIN" && sessionData && (
                   <>
                     <Link
                       href="/restaurantApplications"
