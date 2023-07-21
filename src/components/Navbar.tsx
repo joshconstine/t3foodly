2c67fcf3adec9e9fd2ec07b05abd288a197b0c6e
joshua constine <joshua.constine97@gmail.com>
2023-07-21 11:35:46 -0700

    fixed navbar popper

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = () => {
             <FormatListBulletedIcon sx={{ color: "white" }} />
           </IconButton>
           {expanded && (
-            <div className="fixed  top-16 right-2 rounded-md  border-2 border-black bg-primary sm:-ml-40 md:absolute md:mt-4 ">
+            <div className="  absolute top-16 -ml-40  rounded-md border-2 border-black  bg-primary ">
               <div className="flex flex-col gap-4 p-4">
                 <Link
                   href="/restaurant"
